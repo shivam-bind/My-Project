3f81f4181e636238c6dd1271c56a8cf40086a087
Shivam Bind <kevatshivam216@gmail.com>
2025-08-05 17:32:25 +0530

    add login page

--- webPages/settings.py
+++ webPages/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'webPages.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'webPages' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
